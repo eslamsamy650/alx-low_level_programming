03d39012c231f4aea977b5e15cdda828596ea926
EslamSamy <eslamsamy650@gmail.com>
2023-08-10 03:22:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
 * main - Entry point
 * Description: 'the program print the below sentance'.
 * Return: Always 1(Success)
 */
-int main(void) {
+int main(void)
+{
 char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i;
 for (i = 0; i < strlen(message); i++)
 {
 putchar(message[i]);
 }
-return 1;
+return (1);
 }
