4e1ceea4677fa01cb75ffe820e5deadf14bf4c15
EslamSamy <eslamsamy650@gmail.com>
2023-08-10 02:00:18 -0700

    my first file in the ALX Scolarship

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* The main function is the entry point for your program. */
+/*
+* The main function is the entry point for your program.
+* It only Print The Below Sentance.
+* It takes no parameters and returns an integer.
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
