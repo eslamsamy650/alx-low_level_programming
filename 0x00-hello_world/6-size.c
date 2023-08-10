906d28987a2d54a7911278dc0094939daf1ec8ad
EslamSamy <eslamsamy650@gmail.com>
2023-08-10 02:19:45 -0700

    my Second C file in the ALX Scolarship

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'the program print the below sentance'.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("%lu", sizeof(int));
+return (0);
+}
