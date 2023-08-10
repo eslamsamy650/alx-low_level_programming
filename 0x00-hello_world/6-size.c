8ac70a6aa9dca507cf84d7b5b210ea931321da8f
EslamSamy <eslamsamy650@gmail.com>
2023-08-10 02:23:29 -0700

    my Third C file in the ALX Scolarship

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,15 @@
 */
 int main(void)
 {
-printf("%lu", sizeof(int));
+/* Print the size of the char type. */
+printf("The size of char is %lu bytes.\n", sizeof(char));
+/* Print the size of the int type. */
+printf("The size of int is %lu bytes.\n", sizeof(int));
+/* Print the size of the long int type. */
+printf("The size of long int is %lu bytes.\n", sizeof(long int));
+/* Print the size of the float type. */
+printf("The size of float is %lu bytes.\n", sizeof(float));
+/* Print the size of the double type. */
+printf("The size of double is %lu bytes.\n", sizeof(double));
 return (0);
 }
