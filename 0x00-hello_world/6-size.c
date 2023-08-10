f235891994592be887fb5cd32c9621756af5b2fb
EslamSamy <eslamsamy650@gmail.com>
2023-08-10 02:33:10 -0700

    my Third C file in the ALX Scolarship

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ printf("Size of a int: %lu byte(s)\n", sizeof(int));
 /* Print the size of the long int type. */
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 /* Print the size of the float type. */
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+/* Print the size of the float type. */
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-/* Print the size of the double type. */
-printf("Size of a double: %lu byte(s)\n", sizeof(double));
 return (0);
 }
