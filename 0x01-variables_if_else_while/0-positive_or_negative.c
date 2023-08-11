b3b23e142eb2f39c88f78cac457eb6cffe16c585
EslamSamy <eslamsamy650@gmail.com>
2023-08-11 11:02:09 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ int n;
 scanf("%d", &n);
 if (n > 0)
 {
-printf("is positive");
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-printf("is zero");
+printf("%d is zero", n);
 }
 else if (n < 0)
 {
-printf("is negative");
+printf("%d is negative", n);
 }
 return (0);
 }
