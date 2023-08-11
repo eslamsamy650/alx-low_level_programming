ab8198a74b76c651c8ccb59397c8927980023fd7
EslamSamy <eslamsamy650@gmail.com>
2023-08-11 14:14:44 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i == 99 && j == 98)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
