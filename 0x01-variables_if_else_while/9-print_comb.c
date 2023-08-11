dd2915e5046192cca93d8aa607b3989162ef0b09
EslamSamy <eslamsamy650@gmail.com>
2023-08-11 13:17:21 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,12 @@
 */
 int main(void)
 {
-int i, j;
+int i;
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 10; j++)
-{
-if (i < j)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i != 9 || j != 8)
-{
 putchar(',');
 putchar(' ');
-}
-}
-}
 }
 return (0);
 }
