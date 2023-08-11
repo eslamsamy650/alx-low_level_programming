f3e9f95969c17e5a69aaecf17caea7a548c0010e
EslamSamy <eslamsamy650@gmail.com>
2023-08-11 13:29:52 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (i < j)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 9 || j != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
